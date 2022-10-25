83b5514cf05f48b0d2190bb63fcb95bcf3f7d1dc
Ilnur Gimadi <ilnoor.gimadi@gmail.com>
2022-10-25 16:40:56 +0600

    Update bush1.sh

--- bush1.sh
+++ bush1.sh
@@ -9,3 +9,12 @@ mv map_1.1 map
 mkdir logs
 mv `find . -maxdepth 1 -type f -name "*.log"` logs
 
+pkill -f control.sh
+
+./help/keygen.sh
+cd key
+rm `find . -maxdepth 1 -type f -name "file*"`
+cd ..
+./help/unifier.sh
+rm -R key
+mv main3.key help
